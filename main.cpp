2d2a3944f76be2e14c080740b9c2f46a195b052e
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-11-06 23:13:01 -0300

    Atualizando main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include <time.h>
 #include <windows.h>
 #include "./src/functions/dados.h"
+#include "./src/functions/sortear_jogadores.h"
+
 
 int main () {
     srand(time(NULL));
-    mostrarDados();
     system("pause");
-
     return 0;
 }
