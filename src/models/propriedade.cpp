6021330aa6c05705a3bb391640ea1047acb7b4d0
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-11-09 15:36:19 -0300

    adicionando método receber_aluguel() em propriedades

--- src/models/propriedade.cpp
+++ src/models/propriedade.cpp
@@ -49,9 +49,9 @@ void mostrar_propriedade(PROPRIEDADE *p) {
    cout << "==========================================================" << endl;
    cout << "| Tipo de propriedade: " << p->tipo_propriedade << endl;
    cout << "| " << p->nome_avenida << endl; 
-   cout << "| " << "R$ " << p->valor_aluguel << endl;
+   cout << "| Aluguel: " << "R$ " << p->valor_aluguel << endl;
    if (p->tem_dono == true) {
-       cout << "| " << "Proprietario: " << p->nome_comprador << endl;
+       cout << "| " << "Proprietario(a): " << p->nome_comprador << endl;
    } 
    cout << "| " << "Casas: " << p->casas << " Hoteis: " << p->hoteis << endl;
    cout << "==========================================================" << endl;
@@ -60,31 +60,43 @@ void mostrar_propriedade(PROPRIEDADE *p) {
 
 void comprar_propriedade(PLAYER *pl, PROPRIEDADE *p) {
     if (p->tem_dono) {
-        cout << "Essa propriedade ja tem um dono. Voce nao pode comprar ela!" << endl;
+        cout << "Essa propriedade ja tem dono(a). Voce nao pode comprar ela!" << endl;
     } else {
         p->tem_dono = true;
         pl->carteira -= p->valor_aluguel;
-        cout << "\nJogador " << pl->nome << " comprou a propriedade " << endl;
+        cout << "\nPlayer " << pl->nome << " comprou a propriedade " << endl;
         cout << "Saldo " << pl->nome << ": R$ " << pl->carteira << endl;
         strcpy(p->nome_comprador, pl->nome);
         mostrar_propriedade(p);
     }
 }
 
-/*
+// Função que é ativada quando um player cai em uma propriedade que já tem dono(a)
+void receber_aluguel(PLAYER *pl_caiu, PLAYER *pl_dono, PROPRIEDADE *p) {
+    mostrar_propriedade(p);
+    if (p->tem_dono && (strcmp(p->nome_comprador, pl_dono->nome) == 0)) {
+        cout << "Player " << pl_caiu->nome << " caiu na propriedade do player " << pl_dono->nome << endl;
+        pl_caiu->carteira -= p->valor_aluguel;
+        pl_dono->carteira += p->valor_aluguel;
+        cout << endl;
+        cout << pl_caiu->nome << " pagou R$ " << p->valor_aluguel << " a " << pl_dono->nome << endl;
+        cout << "\nSaldo " << pl_caiu->nome << ": R$ " << pl_caiu->carteira << endl;
+        cout << "Saldo " << pl_dono->nome << ": R$ " << pl_dono->carteira << endl;
+    }
+}
+
 int main() {
     PROPRIEDADE p, *ptr_p;
     ptr_p = &p;
-    PLAYER pl, *ptr_pl;
+    PLAYER pl, *ptr_pl, pl2, *ptr_pl2;
     ptr_pl = &pl;
+    ptr_pl2 = &pl2;
 
-    strcpy(ptr_pl->nome, "Felipe");
-    strcpy(ptr_p->nome_avenida, "Av. Orlando Gomes");
-    ptr_pl->carteira = 3000;
+    pl = criar_player(1, "Felipe", 2, 3000);
+    pl2 = criar_player(1, "Amanda", 1, 7000);
 
-    p = criar_propriedade(1, ptr_p->nome_avenida, false, ptr_pl->nome, 2000, 0, 0);
-    comprar_propriedade(ptr_pl, ptr_p);
+    p = criar_propriedade(1, "Avenida Teste", true, ptr_pl2->nome, 2000, 0, 0);
+    receber_aluguel(ptr_pl, ptr_pl2, ptr_p);
 
     return 0;
 }
-*/
