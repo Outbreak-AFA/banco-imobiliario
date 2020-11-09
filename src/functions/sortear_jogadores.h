2d8315f561f6d88684dd6fe7bdbeb698038a825c
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2020-11-09 12:34:59 -0300

    testando sortear_jogadores.h

--- src/functions/sortear_jogadores.h
+++ src/functions/sortear_jogadores.h
@@ -1,7 +1,6 @@
 #include "dados.h"
 #include <iostream>
 #include <windows.h>
-#include "./src/estruturas/filaencadeada.h"
 #include "./src/models/players.h"
 #include <list>
 #include <vector>
@@ -18,24 +17,24 @@ Essa função recebe a quantidade de jogadores como argumento.
 Por meio de um loop for, serão feitas a jogada de dados de cada jogador.
 Cada valor tirado por jogador, será armazenado em uma list<>.
 */
-int sortearJogadores(vector<PLAYER> &players, int player_count) {
+int sortearJogadores(int *player_count) {
 
-    if (player_count < 2 || player_count > 4) {
+    if (*player_count < 2 || *player_count > 4) {
         cout << "Quantidade de jogadores deve ser no mínimo 2 e no maximo 4!" << endl;
         return 0;
     } else {
 
-        PLAYER p;
+        //PLAYER p;
         list<int> resultado;
 
-        for (int i=0; i < player_count; i++) {
+        for (int i=0; i < *player_count; i++) {
             cout << "Jogador " << i+1 << " vai lançar os dados!" << endl;
             Sleep(1000); // esperar 1 segundo
             int valor_dados = mostrarDados();
             cout << "Jogador " << i+1 << " tirou " << valor_dados << endl;
-            //resultado.push_back(valor_dados);
-            players.at(i).resultado_dados = valor_dados;
-            resultado.push_back(players.at(i).resultado_dados);
+            resultado.push_back(valor_dados);
+            //players.at(i).resultado_dados = valor_dados;
+            //resultado.push_back(players.at(i).resultado_dados);
             // player->resultado = valor_dados
             // resultado.push_back(player->resultado);
             system("pause");
@@ -49,10 +48,14 @@ int sortearJogadores(vector<PLAYER> &players, int player_count) {
 
 }
 
+/*
 int main() {
     int quantidade = 2;
-    PLAYER p;
+    int *ptr_quantidade = &quantidade;
+    PLAYER p, *ptr_p;
+    ptr_p = &p;
     vector<PLAYER> players;
-    sortearJogadores(players, quantidade);
+    sortearJogadores(ptr_quantidade);
     return 0;
 }
+*/
